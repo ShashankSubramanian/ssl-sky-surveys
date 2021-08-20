84197b406f8a5fdafe656bd81f0cdacdc35bd808
Shashank Subramanian <shashank1693@gmail.com>
2021-08-20 11:34:44 -0700

    added the lucidrains config

--- models/vit_dino.py
+++ models/vit_dino.py
@@ -313,6 +313,12 @@ def vit_tiny(patch_size=16, **kwargs):
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
+def vit_lucidrains(patch_size=4, **kwargs):
+    model = VisionTransformer(
+        patch_size=patch_size, embed_dim=512, depth=6, num_heads=8, mlp_ratio=1,
+        qkv_bias=False, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
 def vit_small(patch_size=16, **kwargs):
     model = VisionTransformer(
         patch_size=patch_size, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4,
